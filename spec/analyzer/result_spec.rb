b2cddd8c8e9f9c1c7776d658c6955680cef9c557
Boris Gushin <boris@rocketguys.com>
2019-01-25 23:57:14 +0300

    specs

--- spec/analyzer/result_spec.rb
+++ spec/analyzer/result_spec.rb
@@ -1,2 +1,50 @@
 describe Ticker::Analyzer::Result do
+  let(:date) { '2018-01-01' }
+  let(:start) { 10 }
+  let(:fin) { 12 }
+
+  describe '#negative?' do
+    subject { described_class.new(date, start, fin).negative? }
+
+    it { expect(subject).to be_falsey }
+
+    context 'with downfall' do
+      let(:fin) { 8 }
+
+      it { expect(subject).to be_truthy }
+    end
+  end
+
+  describe '#day' do
+    subject { described_class.new(date, start, fin).day }
+
+    it { expect(subject).to eq(Date.new(2018, 1, 1)) }
+
+    context 'invalid form' do
+      let(:date) { 'test' }
+
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+  end
+
+  describe 'comparison operator' do
+    let(:one) { described_class.new('2018-01-01', 10, 12) }
+    let(:other) { described_class.new('2018-02-03', 13, 15) }
+    subject { (one <=> other) }
+
+    it { expect(subject).to eq(1) }
+
+    context 'reversed' do
+      let(:one) { described_class.new('2018-02-03', 13, 15) }
+      let(:other) { described_class.new('2018-01-01', 10, 12) }
+
+      it { expect(subject).to eq(-1) }
+    end
+  end
+
+  describe '#to_s' do
+    subject { described_class.new(date, start, fin).to_s }
+
+    it { expect(subject).to eq("#{Date.parse(date)}: Closed at #{fin.to_f} (#{start.to_f} ~ #{fin.to_f})")}
+  end
 end
--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -1,2 +1,59 @@
 describe Ticker::Analyzer do
+  let(:retriever) { double(stock_data: enumerator) }
+  let(:enumerator) do
+    [
+      ['2018-01-01', 10, 15],
+      ['2018-01-02', 15, 20],
+      ['2018-01-03', 20, 25],
+      ['2018-01-04', 26, 10],
+      ['2018-01-05', 11, 30],
+      ['2018-01-06', 32, 20]
+    ].each
+  end
+
+  describe '#history' do
+    subject { described_class.new(retriever).history }
+    let(:first) { subject.first }
+    let(:last) { subject.last }
+
+    it { expect(subject).to be_kind_of(Array) }
+    it { expect(first).to be_kind_of(Ticker::Analyzer::Result) }
+    it { expect(first.day).to eq(Date.new(2018, 1, 1)) }
+    it { expect(first.start_price).to eq(10) }
+  end
+
+  describe '#top_drawdowns' do
+    subject { described_class.new(retriever).top_drawdowns }
+
+    it { expect(subject).to be_kind_of(Array) }
+    it { expect(subject.first).to be_kind_of(Ticker::Analyzer::Result) }
+    it { expect(subject.size).to eq(2) }
+    it { expect(subject).to all(be_negative) }
+  end
+
+  describe '#highest_drawdown' do
+    subject { described_class.new(retriever).highest_drawdown }
+
+    it { expect(subject).to be_kind_of(Ticker::Analyzer::Result) }
+    it { expect(subject).to be_negative }
+    it { expect(subject.day).to eq(Date.new(2018, 1, 4)) }
+  end
+
+  describe '#return' do
+    subject { described_class.new(retriever).return }
+
+    it { expect(subject).to be_kind_of(Ticker::Analyzer::Return) }
+  end
+
+  describe '#empty?' do
+    subject { described_class.new(retriever).empty? }
+
+    it { expect(subject).to be_falsey }
+
+    context 'has no data' do
+      let(:enumerator) { [].each }
+
+      it { expect(subject).to be_truthy }
+    end
+  end
 end
